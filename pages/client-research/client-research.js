47e72bc8f3647ae7dda75cf08d0bfe7046dbf479
Solbiati Alessandro <alexsolbiati@hotmail.it>
2017-05-22 16:43:10 +0800

    Add files via upload

---
+++ pages/client-research/client-research.js
@@ -0,0 +1,96 @@
+// pages/client-research/client-research.js
+var pageData = {}
+for (var i = 1; i < 5; ++i) {
+  (function (index) {
+    pageData[`slider${index}change`] = function (e) {
+      console.log(`slider${index}发生change事件，携带值为`, e.detail.value)
+    }
+  })(i);
+}
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  pageData,
+  
+  data: {
+    date: "2017-04-20",
+    start_time: "09:00",
+    end_time: "11:00"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  }
+})
